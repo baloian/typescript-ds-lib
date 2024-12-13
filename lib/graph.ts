6051ac5f74190797fc14a5a24ddc7ca89dc461a4
Artiom Baloian <artiom.baloian@gmail.com>
2024-12-12 23:22:36 -0500

    Made graph weight a template

--- lib/graph.ts
+++ lib/graph.ts
@@ -1,18 +1,14 @@
-import { GraphWeight } from '../types';
-
-
-export interface Graph<T> {
-  addVertex(vertex: T): void;
-  removeVertex(vertex: T): void;
-  addEdge(from: T, to: T, weight?: GraphWeight): void;
-  removeEdge(from: T, to: T): void;
-  getNeighbors(vertex: T): T[];
-  getEdgeWeight(from: T, to: T): GraphWeight;
-  getVertices(): T[];
-  hasVertex(vertex: T): boolean;
-  hasEdge(from: T, to: T): boolean;
+export interface Graph<V, W> {
+  addVertex(vertex: V): void;
+  removeVertex(vertex: V): void;
+  addEdge(from: V, to: V, weight?: W): void;
+  removeEdge(from: V, to: V): void;
+  getNeighbors(vertex: V): V[];
+  getEdgeWeight(from: V, to: V): W;
+  getVertices(): V[];
+  hasVertex(vertex: V): boolean;
+  hasEdge(from: V, to: V): boolean;
   vertexCount(): number;
   edgeCount(): number;
   clear(): void;
 }
-

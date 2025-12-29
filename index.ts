d5dd3cb0b48b8c1daa32ec801ac3d95b86aa7628
Artiom Baloian <artiom.baloian@gmail.com>
2025-12-29 16:24:10 -0500

    Export shuffle

--- index.ts
+++ index.ts
@@ -14,3 +14,4 @@ export { RedBlackTree } from './lib/red-black-tree';
 export { Set } from './lib/set';
 export { Stack } from './lib/stack';
 export { binarySearch } from './algorithms/binary-search';
+export { shuffle } from './algorithms/shuffle';
